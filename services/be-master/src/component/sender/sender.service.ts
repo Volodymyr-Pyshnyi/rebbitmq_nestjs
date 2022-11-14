ff1d6d7530f4eb7c9d917af208ec008f5df960ac
Volodymyr Pyshnyi <volodymyr.pyshnyi@stairwaysoft.com>
2022-11-14 10:18:57 +0200

    Update

--- services/be-master/src/component/sender/sender.service.ts
+++ services/be-master/src/component/sender/sender.service.ts
@@ -1,25 +1,28 @@
-import {Inject, Injectable} from '@nestjs/common';
-import {ClientProxy, RmqRecordBuilder} from '@nestjs/microservices';
-import {lastValueFrom} from 'rxjs';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy, RmqRecordBuilder } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class SenderService {
-  public sum: number = NaN;
+  public sum = NaN;
 
-  constructor(@Inject('CUSTOM_SUM_MICROSERVICE') private client: ClientProxy) {
-  }
+  constructor(@Inject('CUSTOM_SUM_MICROSERVICE') private client: ClientProxy) {}
 
   public async getResult(query) {
-    const record = new RmqRecordBuilder({sum: query.sum.split(',').map(_ => Number(_))})
+    const record = new RmqRecordBuilder({
+      sum: query.sum.split(',').map((_) => Number(_)),
+    })
       .setOptions({
         headers: {
           ['x-version']: '1.0.0',
         },
         priority: 3,
-      }).build();
-    this.sum = await lastValueFrom(this.client.send('microservice_sum', record));
+      })
+      .build();
+    this.sum = await lastValueFrom(
+      this.client.send('microservice_sum', record),
+    );
 
     return `Your request: [${query.sum}]<br/><br/>Your result: ${this.sum}`;
   }
-
 }
